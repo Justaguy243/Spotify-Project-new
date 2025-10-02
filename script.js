d77c8718ab94d37a7ff41509fa2f8be9d2db752d
Just A Guy 404 <mirajmeshram243@gmail.com>
2025-10-02 13:35:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ const Removestyle2 = (e) => {
 }
 
 const Getimage = (image) => {
-  let haveimage = "spotify_images/" + image.replaceAll("%20", " ").replaceAll(".mp3", ".jpg");
+  let haveimage = "spotify79/spotify_images/" + image.replaceAll("%20", " ").replaceAll(".mp3", ".jpg");
   return haveimage;
 }
 
